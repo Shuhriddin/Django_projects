2b5115d940bb1dad627a3980f4713ade37d7cfd7
shuxriddin <shuxriddinsolixov2023@gmail.com>
2024-08-21 11:39:41 +0500

    To-Do

---
+++ To-Do-App/.venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\KOD\PYTHON\GitHub_SENDING\Django_Projects\To-Do-App\.venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
