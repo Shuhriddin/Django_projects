7b82b7503039b538871eb3d4231879a899a4dad0
Shuhriddin Solixov <152378280+Shuhriddin@users.noreply.github.com>
2024-09-12 10:06:35 +0500

    Search Fields

---
+++ 004.SearchFields/app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Data
+from django.db.models import Q
+# Create your views here.
+
+
+def index(request):
+    if 'q' in request.GET:
+        q = request.GET['q']
+        # data = Data.objects.filter(first_name__icontains=q)
+        multiple_q = Q(Q(first_name__icontains=q) | Q(last_name__icontains=q) | Q(age__icontains=q))  # Ism, Familiya, Yosh bo`yicha qidiradi
+        data = Data.objects.filter(multiple_q)
+    else:
+        data = Data.objects.all()
+    context = {
+        'data': data
+    }
+    return render(request, 'app/index.html', context)
